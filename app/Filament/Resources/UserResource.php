c7f095919f1581d3b5db7ea8cb1b642b0d1fb88f
Blasta 94 <gianlucaidili94@gmail.com>
2023-03-19 05:20:18 +0100

    finito pannello user personalizzato

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\User;
+use Filament\Tables;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\DeleteAction;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Tables\Actions\DeleteBulkAction;
+use App\Filament\Resources\UserResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\Pages\EditUser;
+use App\Filament\Resources\UserResource\Pages\ListUsers;
+use App\Filament\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Filament\Resources\UserResource\RelationManagers\RolesRelationManager;
+use Filament\Pages\Page;
+use Filament\Tables\Filters\TrashedFilter;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    protected static ?string $navigationGroup = 'Admin Management';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Toggle::make('is_admin')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->maxLength(255)
+                    ->dehydrateStateUsing(static fn(null|string $state): 
+                        null|string =>
+                        filled($state) ? Hash::make($state): null,
+                    )->required(static fn(Page $livewire): string =>
+                        $livewire instanceof CreateUser,
+                    )->dehydrated(static fn(null|string $state):bool =>
+                        filled($state),
+                    )->label(static fn(Page $livewire): string =>
+                        ($livewire instanceof EditUser) ? 'New Password' : 'Password'
+                    ),
+                    CheckboxList::make('roles')
+                        ->relationship('roles', 'name')
+                        ->columns(2)
+                        ->helperText('Only Choose One!')
+                        ->required()
+                    // BelongsToMany::make('Roles')
+                    //     ->options(Role::pluck('roles', 'name'))
+                    //     ->help('Only Choose One!')
+                    //     ->rules('required')
+                    //     ->columns(2)
+                    //     ->helperText('Only Choose One!')
+                    //     ->required()
+            ]);
+
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\IconColumn::make('is_admin')
+                    ->boolean()
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('roles.name')->sortable()->searchable(),  
+                Tables\Columns\TextColumn::make('email')
+                ->sortable()
+                ->searchable(),
+                // Tables\Columns\TextColumn::make('email_verified_at')
+                //     ->dateTime(),
+                // Tables\Columns\TextColumn::make('two_factor_secret'),
+                // Tables\Columns\TextColumn::make('two_factor_recovery_codes'),
+                // Tables\Columns\TextColumn::make('two_factor_confirmed_at')
+                //     ->dateTime(),
+                // Tables\Columns\TextColumn::make('current_team_id'),
+                // Tables\Columns\TextColumn::make('profile_photo_path'),
+                TextColumn::make('deleted_at')
+                ->dateTime('d-M-Y')
+                ->sortable()
+                ->searchable(),
+                TextColumn::make('created_at')
+                ->dateTime('d-M-Y')
+                ->sortable()
+                ->searchable(),
+                // Tables\Columns\TextColumn::make('updated_at')
+                //     ->dateTime(),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            RolesRelationManager::class,
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }    
+}
